f9d4ce778f5a595b7b8277dad7ca2b8ee8e6d62c
viswanathavvaru <34683718+viswanathavvaru@users.noreply.github.com>
2024-01-17 22:07:52 +0530

    Update WebapplicationApplication.java

--- src/main/java/example/webapplication/WebapplicationApplication.java
+++ src/main/java/example/webapplication/WebapplicationApplication.java
@@ -2,12 +2,20 @@ package example.webapplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
-public class WebapplicationApplication {
+@RequestMapping
+public class ApplicationApplication {
 
+
+	@GetMapping("/")
+	public String hello() {
+		return "welcome to springboot application";
+	}
 	public static void main(String[] args) {
-		SpringApplication.run(WebapplicationApplication.class, args);
+		SpringApplication.run(ApplicationApplication.class, args);
 	}
 
 }
